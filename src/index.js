8adc957316f49d87bc51ab514018bbdccaebe300
davi-souza2001 <davisouza2001dv@gmail.com>
2021-07-08 20:43:15 -0300

    best config

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Form from './components/Form';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Form />
   </React.StrictMode>,
   document.getElementById('root')
 );
