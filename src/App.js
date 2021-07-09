54d6d95d46b81c3d8b3e850a35396d2a984542ad
davi-souza2001 <davisouza2001dv@gmail.com>
2021-07-08 21:53:32 -0300

    Update Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 import Form from './components/Form';
+import Msg from './components/Msg';
 
 function App() {
   return (
-    <Form/>
+    
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Form}/>
+        <Route path="/mensage" component={Msg}/>
+      </Switch>
+    </Router>
+
+    
   );
 }
 
---
+++ src/components/Msg.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Msg = () => {
+    return(
+        <div>
+            Ola
+        </div>
+    );
+}
+
+export default Msg;
